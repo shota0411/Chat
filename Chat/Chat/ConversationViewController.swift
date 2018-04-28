362de87b647c3dc79172d871eced45b00de1cf18
松田 翔大 <thank2626@gmail.com>
2018-04-29 05:19:16 +0900

    MessageKitのsampleを移管（データを入れてないからかクラッシュする）

--- Chat/Chat/ConversationViewController.swift
+++ Chat/Chat/ConversationViewController.swift
@@ -1,26 +1,42 @@
-//
-//  ViewController.swift
-//  Chat
-//
-//  Created by 松田翔大 on 2018/04/29.
-//  Copyright © 2018年 松田翔大. All rights reserved.
-//
+/*
+ MIT License
+ 
+ Copyright (c) 2017-2018 MessageKit
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 import UIKit
 import MessageKit
 import MapKit
 
-class ViewController: MessagesViewController {
-    
+class ConversationViewController: MessagesViewController {
+
     let refreshControl = UIRefreshControl()
     
     var messageList: [MockMessage] = []
     
     var isTyping = false
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         let messagesToFetch = UserDefaults.standard.mockMessagesCount()
         
         DispatchQueue.global(qos: .userInitiated).async {
@@ -32,13 +48,13 @@ class ViewController: MessagesViewController {
                 }
             }
         }
-        
+
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
         messagesCollectionView.messageCellDelegate = self
         messageInputBar.delegate = self
-        
+
         messageInputBar.sendButton.tintColor = UIColor(red: 69/255, green: 193/255, blue: 89/255, alpha: 1)
         scrollsToBottomOnKeybordBeginsEditing = true // default false
         maintainPositionOnKeyboardFrameChanged = true // default false
@@ -84,7 +100,7 @@ class ViewController: MessagesViewController {
             messageInputBar.backgroundColor = messageInputBar.backgroundView.backgroundColor
             
         }
-        
+
     }
     
     @objc func loadMoreMessages() {
@@ -127,7 +143,7 @@ class ViewController: MessagesViewController {
     }
     
     // MARK: - Keyboard Style
-    
+
     func slack() {
         defaultStyle()
         messageInputBar.backgroundView.backgroundColor = .white
@@ -239,25 +255,25 @@ class ViewController: MessagesViewController {
 
 // MARK: - MessagesDataSource
 
-extension ViewController: MessagesDataSource {
-    
+extension ConversationViewController: MessagesDataSource {
+
     func currentSender() -> Sender {
         return SampleData.shared.currentSender
     }
-    
+
     func numberOfMessages(in messagesCollectionView: MessagesCollectionView) -> Int {
         return messageList.count
     }
-    
+
     func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
         return messageList[indexPath.section]
     }
-    
+
     func cellTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
         let name = message.sender.displayName
         return NSAttributedString(string: name, attributes: [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: .caption1)])
     }
-    
+
     func cellBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
         
         struct ConversationDateFormatter {
@@ -271,47 +287,47 @@ extension ViewController: MessagesDataSource {
         let dateString = formatter.string(from: message.sentDate)
         return NSAttributedString(string: dateString, attributes: [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: .caption2)])
     }
-    
+
 }
 
 // MARK: - MessagesDisplayDelegate
 
-extension ViewController: MessagesDisplayDelegate {
-    
+extension ConversationViewController: MessagesDisplayDelegate {
+
     // MARK: - Text Messages
-    
+
     func textColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
         return isFromCurrentSender(message: message) ? .white : .darkText
     }
-    
+
     func detectorAttributes(for detector: DetectorType, and message: MessageType, at indexPath: IndexPath) -> [NSAttributedStringKey : Any] {
         return MessageLabel.defaultAttributes
     }
-    
+
     func enabledDetectors(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> [DetectorType] {
         return [.url, .address, .phoneNumber, .date]
     }
-    
+
     // MARK: - All Messages
     
     func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
         return isFromCurrentSender(message: message) ? UIColor(red: 69/255, green: 193/255, blue: 89/255, alpha: 1) : UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
     }
-    
+
     func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle {
         let corner: MessageStyle.TailCorner = isFromCurrentSender(message: message) ? .bottomRight : .bottomLeft
         return .bubbleTail(corner, .curved)
-        //        let configurationClosure = { (view: MessageContainerView) in}
-        //        return .custom(configurationClosure)
+//        let configurationClosure = { (view: MessageContainerView) in}
+//        return .custom(configurationClosure)
     }
     
     func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
         let avatar = SampleData.shared.getAvatarFor(sender: message.sender)
         avatarView.set(avatar: avatar)
     }
-    
+
     // MARK: - Location Messages
-    
+
     func annotationViewForLocation(message: MessageType, at indexPath: IndexPath, in messageCollectionView: MessagesCollectionView) -> MKAnnotationView? {
         let annotationView = MKAnnotationView(annotation: nil, reuseIdentifier: nil)
         let pinImage = #imageLiteral(resourceName: "pin")
@@ -319,7 +335,7 @@ extension ViewController: MessagesDisplayDelegate {
         annotationView.centerOffset = CGPoint(x: 0, y: -pinImage.size.height / 2)
         return annotationView
     }
-    
+
     func animationBlockForLocation(message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> ((UIImageView) -> Void)? {
         return { view in
             view.layer.transform = CATransform3DMakeScale(0, 0, 0)
@@ -339,12 +355,12 @@ extension ViewController: MessagesDisplayDelegate {
 
 // MARK: - MessagesLayoutDelegate
 
-extension ViewController: MessagesLayoutDelegate {
-    
+extension ConversationViewController: MessagesLayoutDelegate {
+
     func avatarPosition(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> AvatarPosition {
         return AvatarPosition(horizontal: .natural, vertical: .messageBottom)
     }
-    
+
     func messagePadding(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIEdgeInsets {
         if isFromCurrentSender(message: message) {
             return UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 4)
@@ -352,7 +368,7 @@ extension ViewController: MessagesLayoutDelegate {
             return UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 30)
         }
     }
-    
+
     func cellTopLabelAlignment(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> LabelAlignment {
         if isFromCurrentSender(message: message) {
             return .messageTrailing(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10))
@@ -360,7 +376,7 @@ extension ViewController: MessagesLayoutDelegate {
             return .messageLeading(UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0))
         }
     }
-    
+
     func cellBottomLabelAlignment(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> LabelAlignment {
         if isFromCurrentSender(message: message) {
             return .messageLeading(UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0))
@@ -368,68 +384,68 @@ extension ViewController: MessagesLayoutDelegate {
             return .messageTrailing(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10))
         }
     }
-    
+
     func footerViewSize(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGSize {
-        
+
         return CGSize(width: messagesCollectionView.bounds.width, height: 10)
     }
-    
+
     // MARK: - Location Messages
-    
+
     func heightForLocation(message: MessageType, at indexPath: IndexPath, with maxWidth: CGFloat, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
         return 200
     }
-    
+
 }
 
 // MARK: - MessageCellDelegate
 
-extension ViewController: MessageCellDelegate {
-    
+extension ConversationViewController: MessageCellDelegate {
+
     func didTapAvatar(in cell: MessageCollectionViewCell) {
         print("Avatar tapped")
     }
-    
+
     func didTapMessage(in cell: MessageCollectionViewCell) {
         print("Message tapped")
     }
-    
+
     func didTapTopLabel(in cell: MessageCollectionViewCell) {
         print("Top label tapped")
     }
-    
+
     func didTapBottomLabel(in cell: MessageCollectionViewCell) {
         print("Bottom label tapped")
     }
-    
+
 }
 
 // MARK: - MessageLabelDelegate
 
-extension ViewController: MessageLabelDelegate {
-    
+extension ConversationViewController: MessageLabelDelegate {
+
     func didSelectAddress(_ addressComponents: [String : String]) {
         print("Address Selected: \(addressComponents)")
     }
-    
+
     func didSelectDate(_ date: Date) {
         print("Date Selected: \(date)")
     }
-    
+
     func didSelectPhoneNumber(_ phoneNumber: String) {
         print("Phone Number Selected: \(phoneNumber)")
     }
-    
+
     func didSelectURL(_ url: URL) {
         print("URL Selected: \(url)")
     }
-    
+
 }
 
 // MARK: - MessageInputBarDelegate
 
-extension ViewController: MessageInputBarDelegate {
-    
+extension ConversationViewController: MessageInputBarDelegate {
+
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
         
         // Each NSTextAttachment that contains an image will count as one empty character in the text: String
@@ -456,6 +472,5 @@ extension ViewController: MessageInputBarDelegate {
         inputBar.inputTextView.text = String()
         messagesCollectionView.scrollToBottom()
     }
-    
-}
 
+}
